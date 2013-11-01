2681aeef5a3db22cdc814541d5294060635250d1
adeiji <adebayoiji@gmail.com>
2013-11-01 11:43:15 -0700

    Made changes to the scheme.

---
+++ Profit Management Calculator Tests/Profit_Management_Calculator_Tests.m
@@ -0,0 +1,34 @@
+//
+//  Profit_Management_Calculator_Tests.m
+//  Profit Management Calculator Tests
+//
+//  Created by Ade on 10/29/13.
+//  Copyright (c) 2013 Ade. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Profit_Management_Calculator_Tests : XCTestCase
+
+@end
+
+@implementation Profit_Management_Calculator_Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
